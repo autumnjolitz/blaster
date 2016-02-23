2fd81f2c9a0f8a7a8b2a367c76ecf61a773b2764
Autumn Jolitz <autumn.jolitz@gmail.com>
2016-02-22 16:20:17 -0800

    Organize

--- src/hello.c
+++ src/hello.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
-#include "contrib/http_parser.h"
+#include <contrib/http_parser.h>
 
 #ifdef DEBUG
 #define DEBUG_PRINTF(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false )
@@ -14,76 +14,82 @@
 #endif
 
 typedef struct REQUEST {
-    char path[1024];
-    int64_t path_length;
-    bool keep_alive;
+    char *path; //4 or 8 bytes
+    int *path_length; // 4 or 8 bytes
+    struct http_parser_url *url_parser; // 4 or 8 bytes
+    bool *keep_alive; // 1 bytes
 } REQUEST;
 
 int url_ready(http_parser* parser, const char *url, size_t length) {
     REQUEST* request = (REQUEST* )parser->data;
 
-    struct http_parser_url url_parser;
-    http_parser_url_init(&url_parser);
-    http_parser_parse_url(url, length, parser->method == HTTP_CONNECT, &url_parser);
+    http_parser_parse_url(url, length, parser->method == HTTP_CONNECT, request->url_parser);
 
-    uint16_t offset = url_parser.field_data[UF_PATH].off;
-    uint16_t path_length = url_parser.field_data[UF_PATH].len;
+    uint16_t offset = (request->url_parser)->field_data[UF_PATH].off;
+    uint16_t path_length = (request->url_parser)->field_data[UF_PATH].len;
 
-    request->path_length = length;
-    memcpy(request->path + offset, url, path_length);
+    *request->path_length = length;
+    memcpy(request->path, url + offset, path_length);
     return 0;
 }
 
 int on_headers_ready(http_parser* parser) {
     REQUEST* request = (REQUEST* )parser->data;
-    request->keep_alive = (bool) http_should_keep_alive(parser);
+    *(request->keep_alive) = (bool) http_should_keep_alive(parser);
     return 0;
 }
 
 char no_keep_alive[] = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nConnection: close\r\n\r\nOk\n";
-char keep_alive_capable[] = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nKeep-Alive: timeout=15, max=1000\r\nConnection: keep-alive\r\n\r\nOk\n";
+char keep_alive_capable[] = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nKeep-Alive: timeout=15, max=200\r\nConnection: keep-alive\r\n\r\nOk\n";
 
 
 coroutine void handle_request(tcpsock client) {
-    REQUEST request = {"", -1, false};
-    char buf[512];
+    char path[2048] = {0};
+    char buf[8192];
+    struct http_parser_url url_parser = {};
+    http_parser_url_init(&url_parser);
+    int path_length = -1;
+    bool keep_alive_request = false;
+
+    REQUEST request = {path, &path_length, &url_parser, &keep_alive_request};
     int64_t t_s = 0;
-    int64_t reqs_left = 999;
+    int reqs_left = 199;
     size_t num_bytes;
     size_t num_bytes_parsed;
     http_parser_settings settings;
     http_parser parser;
     http_parser_init(&parser, HTTP_REQUEST);
-    parser.data = &request;
 
+    parser.data = &request;
     settings.on_url = url_ready;
     settings.on_headers_complete = on_headers_ready;
 
     begin:
-    request.path_length = -1;
+    path_length = -1;
+
     while(true) {
         int64_t deadline = now() + 5;
         num_bytes = tcprecv(client, buf, sizeof(buf), deadline);
         if (num_bytes == 0) {
             break;
         }
-        t_s = now();
         if (errno != ETIMEDOUT)
         {
             if (errno == ECONNRESET) {
-                DEBUG_PRINTF("RESET! %zu\n", num_bytes);
+                DEBUG_PRINTF("RESET! reqs left: %i bytes: %zu data: %s\n", reqs_left, num_bytes, buf);
                 goto cleanup;
             }
             DEBUG_PRINTF("Errno %d\n", errno);
             goto cleanup;
         }
+        t_s = now();
         num_bytes_parsed = http_parser_execute(&parser, &settings, buf, num_bytes);
         if (num_bytes_parsed == 0)
             break;
     }
     num_bytes_parsed = http_parser_execute(&parser, &settings, buf, num_bytes);
-    if (request.path_length > -1) {
-        if (request.keep_alive) {
+    if (path_length > -1) {
+        if (keep_alive_request) {
             tcpsend(client, keep_alive_capable, sizeof(keep_alive_capable), -1);
             tcpflush(client, -1);
             if ((now() - t_s) > 15*1000 || reqs_left == 0) {
